7c7cdb3013ff2cb4c70fe17bff8551983072c272
Kuhrt Cowan <kuhrt.cowan@ttu.edu>
2017-07-20 17:10:05 -0500

    Finishing home styles

--- index.php
+++ index.php
@@ -48,8 +48,10 @@
                 </div>
                 <h2>Facilities</h2>
                 <p class="rec-home__giant-letter">F</p>
-                <p>Spicy jalapeno bacon ipsum dolor amet doner spare ribs salami, t-bone flank turkey brisket. Shoulder boudin shankle, filet mignon doner ground round turkey turducken t-bone. Boudin alcatra cow rump. Salami flank kevin, cupim boudin t-bone biltong ham hock short loin strip steak frankfurter.</p>
-                <p>Strip steak jerky landjaeger corned beef pig pork chop bresaola salami. Prosciutto pancetta chuck hamburger salami pork ground round spare ribs short loin flank pork belly pig meatloaf tri-tip. Cow short loin meatball, shankle shank ham hock picanha tongue turkey tail pork belly chicken fatback pig. Jerky t-bone corned beef chicken chuck salami, ground round pork belly ball tip bresaola spare ribs pancetta pork chop flank. Pork chop pork belly ribeye, landjaeger burgdoggen andouille tongue spare ribs short loin salami kevin. Fatback salami leberkas tenderloin porchetta ham hock. Shankle tri-tip flank picanha.</p>
+                <div class="rec-home-facilities__text">
+                    <p>Spicy jalapeno bacon ipsum dolor amet doner spare ribs salami, t-bone flank turkey brisket. Shoulder boudin shankle, filet mignon doner ground round turkey turducken t-bone. Boudin alcatra cow rump. Salami flank kevin, cupim boudin t-bone biltong ham hock short loin strip steak frankfurter.</p>
+                    <p>Strip steak jerky landjaeger corned beef pig pork chop bresaola salami. Prosciutto pancetta chuck hamburger salami pork ground round spare ribs short loin flank pork belly pig meatloaf tri-tip. Cow short loin meatball, shankle shank ham hock picanha tongue turkey tail pork belly chicken fatback pig. Jerky t-bone corned beef chicken chuck salami, ground round pork belly ball tip bresaola spare ribs pancetta pork chop flank. Pork chop pork belly ribeye, landjaeger burgdoggen andouille tongue spare ribs short loin salami kevin. Fatback salami leberkas tenderloin porchetta ham hock. Shankle tri-tip flank picanha.</p>
+                </div>
             </section>
 
             <section class="rec-home__sliders">
