a67f16f27938d631d2c5fbdadda87cd0ff12dd39
Devion Villegas <devion.villegas@ttu.edu>
2018-03-21 15:42:44 -0500

    event page templates for lunar lope and kids tri

---
+++ events/kidstriathlon.php
@@ -0,0 +1,238 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include '../includes/ttu-head.html'; ?>
+        <title>Kids Triathlon | Rec Center | TTU</title>
+        <meta name="Description" content="">
+    </head>
+    <body>
+        <?php include '../includes/ttu-body-top.php'; ?>
+
+
+
+
+        <!-- CONTENT START -->
+        <section class="rec-header">
+            <div class="rec-header__background">
+              <div class="top-left-triangle"></div>
+            </div>
+            <h1>Kids Triathlon</h1>
+            <div class="rec-header__image">
+              <div class="top-left-triangle">
+                <div class="triangle-image" style="background-image: url('/images/ttu-aerial-1.jpg');"></div>
+              </div>
+            </div>
+        </section>
+        <section class="rec-sidebar">
+
+        </section>
+
+        <section class="rec-body">
+            <section>
+              <h2>Kid's Triathlon</h2>
+              <div class="flexslider">
+                <ul class="slides">
+                    <li data-thumb="https://res.cloudinary.com/devhound/image/upload/c_scale,w_900/v1519423706/ssc-2_tiwfef.jpg">
+                        <img src="https://res.cloudinary.com/devhound/image/upload/c_scale,w_900/v1519423706/ssc-2_tiwfef.jpg" />
+                    </li>
+                    <li data-thumb="https://res.cloudinary.com/devhound/image/upload/c_scale,w_900/v1519423706/ssc-1_ngoghj.jpg">
+                        <img src="https://res.cloudinary.com/devhound/image/upload/c_scale,w_900/v1519423706/ssc-1_ngoghj.jpg" />
+                    </li>
+                    <li data-thumb="https://res.cloudinary.com/devhound/image/upload/c_scale,w_900/v1519423707/ssc-3_trvvsc.jpg">
+                        <img src="https://res.cloudinary.com/devhound/image/upload/c_scale,w_900/v1519423707/ssc-3_trvvsc.jpg" />
+                    </li>
+                   
+                </ul>
+            </div>
+
+            <h3>Date:</h3>
+            <p>July 14, 2018</p>
+
+            <h3>Time</h3>
+            <p>Starts at 7:30am at the Leisure Pool at Texas Tech University Rec Center</p>
+
+            <h3>Deadline to register:</h3>
+            <p>July 10, without a late fee</p>
+ 
+            <h3>What is a Triathlon?</h3>
+            <p>This event begins with a swim at the Texas Tech Leisure Pool, then transitions to a bike ride and ends with a run around the Student Recreation Center. Age groups will have different distance to compete in:</p>
+
+          <table style="width:100%;border-collapse:collapse;table-layout:fixed;text-align:center;">
+          <tr>
+            <td>Ages</td>
+            <td>Swim</td>
+            <td>Bike</td>
+            <td>Run</td>
+          </tr>
+          <tr>
+            <td>7 - 10 Year Olds</td>
+            <td>100 yds</td>
+            <td>2.4 miles</td>
+            <td>1 mile</td>
+          </tr>
+          <tr>
+            <td>11 - 14 Year Olds</td>
+            <td>200 yds</td>
+            <td>3.2 miles</td>
+            <td>1.5 miles</td>
+          </tr>
+        </table>
+
+        <p>As part of the registration, you will be asked to record your child's swim time to make the event go smoothly. Here are some suggested swim times if you are not able to get your child's time before the event - <a href="Kids Triathlon/17/Average Swim TImes - Kids Tri.pdf" class="pdf">Suggested Childrens Swim Times</a></p>
+
+        <h3>Where is the race being held?</h3>
+        <p>At the Texas Tech Student Recreation and Leisure Pool which is located on Main Street just east of Indiana Avenue. If you are familiar with Lubbock, we share the parking lot with the United Spirit Arena.</p>
+
+        <h3>Race Maps</h3>
+        <p><a href="Kids Triathlon/16/Kids Tri Bicycle Route - July 2010.pdf" class="pdf">Bike Route</a></p>
+
+        <p><a href="Kids Triathlon/16/running map both ages 2016.pdf" class="pdf">Running Route for Both Age Groups</a></p>
+
+        <h3>Registration </h3>
+        <ul>
+          <li>You can register in the Fit/Well at the TTU Rec Center</li>
+          <li><p>Register online for free at <a href="https://register.recsports.ttu.edu/" class="external">http://register.recsports.ttu.edu</a>. For instructions on how to register online, <a href="Kids Triathlon/17/Fusion online registration Kids Triathlon.pdf">please click here.</a></p></li>
+          <li><p>Print <a href="Kids Triathlon/17/Kids Tri Final Brochure Final May 3 .pdf" class="pdf">registration form</a> and return to Rec Sports or mail it in to:</p></li>
+        </ul>
+        <p>Mailing Address:<br />
+          TTU Kids Triathlon<br />
+          Box 42151<br />
+          Lubbock TX  79409
+          <br />
+        </p>
+
+        <p>$20 for the first child in a family and $15 for each additional child in that family.</p>
+        <p> $5 late fee after Monday, July 10 and you won’t be seeded in the swim.          </p>
+        <h3>Triathlon Workshop</h3>
+        <ul>
+          <li><p>Monday, July 9, 6:00pm - 7:00pm | FREE!</p></li>
+          <li><p>Register online for free at <a href="https://register.recsports.ttu.edu" class="external">http://register.recsports.ttu.edu</a> or call 742-3828</p></li>
+        </ul>
+
+        <p>Get a feel for the real thing! Bring your bike, helmet and running shoes to learn how to transition from one race segment to the other. Call 742-3828 to register.</p>
+
+        <h3>Packet Pickup/Last Chance Registration</h3>
+<p> Friday, July 14th 4:00 - 8:00pm 
+          at the Fit/Well inside the Student Rec Center. 
+          <span style="text-transform:uppercase;">All participants must pick up numbers, etc.</span> There will be no race day packet pickup.</p>
+<p>Can't make it? Have a friend or a fellow triathlete pick up the packet!</p>
+<h3>Sponsors</h3>
+<p>The Body Mind Initiative, along with Covenant Health System have been gracious enough to provide sponorship for the 2018 Fit 4 Fun Kid's Triathlon.</p>
+
+<h3> How does a Triathlon work?</h3>
+<p>A triathlon has 3 events with a transition area in between to change equipment and clothes.          </p>
+<p><strong>The Swim:</strong><br /> 
+  Swimmers will go off 1 by 1 at 15 second intervals. They go in the water fastest swimmer to slowest swimmer. That is why we ask for a seed time.  The more accurate the seed time you give us, the less chance of passing/being passed (and less waves). They will push off the wall for the start (no diving). They will swim up one lane, flip (or duck) under the lane line and swim back the next lane. The pool is 25 yards long and is 4 feet, 6 inches deep. The younger age group will swim 4 lengths and the older group will swim 8 lengths. They will exit the pool at a ladder and there will be carpet leading to the exit for them to follow. There will be a time gap between the age groups to give the younger age groups time to complete the cycling before the older group begins. How long will depend on the number of entries. </p>
+<p><strong>First transition:          </strong><br />
+  Participants run (carefully because they will not have shoes on) following the carpet to the transition area where their bike is and the pile of clothes that they want to wear. Parents and other helpers are not allowed in the transition area after the triathlon begins. Children must change clothes, put on shoes etc before mounting the bike. Helmets must be BUCKLED and bikes must be WALKED out of the transition area. There will be a red tape line on the driveway indicating where they can get on their bikes.</p>
+<p> <strong>Cycling: </strong><br />
+Please see the map shown for bike course.  Participants will ride the emergency road entrance and turn right onto Main Street (road will be closed to traffic), turn right onto Flint and right onto 18th Street (roads are not closed to traffic) right onto Indiana and right on Main Street (roads closed to traffic). There will be police at the corners.  7-10 age group does 3 laps; 11-14 age group does 4 laps.  Counting laps is up to participants.  On the last lap, participants will turn right onto the emergency road back towards the transition.  They must dismount their bikes at the read line and walk/jog it back to its spot in the transition. Here are a few hints:
+</p>
+<ul>
+  <li>Make sure shoe laces are tucked in</li>
+  <li> Keep helmet on and buckled at all times  </li>
+  <li>Follow police and corner marshal directions</li>
+  <li> Ride to the right of the street, but watch out for the gravel.</li>
+  <li> Pass on the left, but not in the center of the road</li>
+  <li> Find a grown up if you decide to stop, or need help</li>
+  <li> Walk bike back into the bike area from the red line </li>
+</ul>
+<p><strong>The 2nd Transition:
+          </strong><br />
+        After the bike has been racked and the helmet off, the triathletes will begin their run. Please see the attached map for directions. There will be cups of water available for participants as they leave the transition area to the run. </p>
+<p><strong>The Run:  </strong><br />
+  The running event is on the jogging track/sidewalk around the Rec Center Both courses are an out an back run.  Runners need to stay on the left side of the jogging track.  The turn around point for each will be a Y in the road.  Please see the map.  Every finisher will be awarded a medal as they cross the finish line. No one can run with the child. </p>
+<p><strong>The End:</strong> <br />
+  There will be refreshments for participants. We encourage you to stay and cheer on others. Drawings for prizes will be held after everyone is finished. For the safety and fairness of all participants, please do not remove bikes from the transition area until the last triathlete has begun the run.          </p>
+<h3>What do I need to bring?</h3>
+<ul>
+  <li>  A swim suit. Goggles are not mandatory but most swimmers like them. (no flippers, kickboards or flotation devices allowed)
+  </li>
+  <li>A bike that has working brakes and an approved bike helmet that fits your head. Sorry, training wheels are not allowed.</li>
+  <li> Running shoes          </li>
+</ul>
+<h3>How much can parents help?</h3>
+<p> The best help that parents can give is in making sure your child is ready to compete. </p>
+<ul>
+  <li>Make sure their bike is in working order (especially the brakes) and that their helmet fits. We anticipate kids to be on kid’s bikes—a racing bike is not expected—so please don’t feel intimidated but make sure it is a safe bicycle!          </li>
+  <li>Take them swimming to make sure they can complete the distance. No flotation devices are allowed. The pool depth is 4 feet, 6 inches. They can rest at the ends of the pool if necessary. You might swim with them in a lane and make some splashes because this will probably occur.</li>
+  <li> Find a bike course around your neighborhood of a similar distance (or ride at Tech in the evenings/weekend if you live in Lubbock). Make sure they understand that they need to stay to the right side of the road (with traffic). Teach them that if they want to pass another cyclist, they should pass on the left of the other cyclist but fairly close—not in the middle of the road. Follow behind them on your bike as they get better to let them lead and gently remind them when they veer to the left or right. Get them to tuck their shoes lace bows into the laces or the outside of their shoes so the laces will not get caught in chain ring. <strong>They will make many right turns so please  practice these.  Make sure that their  inside pedal is NOT in the 6 o’clock position as they make the turn (so it  won’t catch on the ground).</strong></li>
+<li> Run with them on a similar distance course in the neighborhood, at a track (or run at Tech in the evenings/weekend if you live in Lubbock).</li>
+  <li> For all of these activities, please start early and work up to the distances. Then try to combine 2 of the activities at one time and, if possible, do all 3 at least once before the triathlon. Give them positive encouragement. Let them figure out what clothes they are going to wear, how they are going to change (i.e. I am going to put my glasses in my shoes so they won’t get broken; I am going to stack my shorts on my shoes with my shirt on top) and practice transitioning from one event to another.</li>
+  <li>  When you come to pick up your packet (if not before), please drive (or ride) them on the cycling course and drive, walk or run the running course so they will feel comfortable knowing where they are going. We will have plenty of volunteers along the way, but they will be more confident if they know where they are going and “how much further” until the end.  </li>
+</ul>
+<h3>Pre-Race Night Preparation </h3>
+<ul>
+  <li> You will find numbers in your packet.</li>
+  <li>  The small one goes on the right front of their helmet and the cardboard one goes on the front of their handlebars.</li>
+  <li> The running number goes on the front of their shirt.</li>
+  <li> If your child is going to compete in just his/her swim suit, you will have to have some sort of elastic belt with the number attached to it that they can slip around their waist for the run. ALL PARTICIPANTS MUST HAVE A NUMBER ON FOR THE RUN.          </li>
+  <li>Put all numbers on clothing, bike the night before.Try to get your child to bed early as it will be an early awakening on race day.</li>
+  <li>  Have the car packed as much as possible.</li>
+</ul>
+<h3> Helpful Transition and Nutrition Hints  </h3>
+<p>These great tips and hints are provided courtesy of Tim Key, Certified Personal Trainer and Triathlete
+  Triathlon Hints<a href="Kids Triathlon/2010/triathlon hints-Tim Key.pdf" class="pdf"> Please Click Here</a></p>
+<h3>Race Day</h3>
+<ul>
+  <li>  Get up in plenty of time to eat a light breakfast and get to the Leisure Pool on time.          You should plan to be in the parking lot no later than 6:45 – 6:50 am.</li>
+  <li>You may help your child place his/her bike in the transition area and get clothes ready. <strong>Make sure they can identify where their bike is when they come out of  the swim.</strong>Bring them to the marking station.</li>
+  <li>See that they have been to the bathroom.          </li>
+  <li> Get them into the pool area by 7:15, give them a kiss and a wish for a fun time! They are on their own from here!</li>
+  <li> You can cheer them on, watch them transition and walk to different points to watch them. Please do not follow in a car or bike on the cycling section. It will be difficult for you to drive to the center of campus to watch that part of the cycling. The run is on the jogging track that surrounds the Rec—you can watch but cannot run with them. We will have light refreshments after the race. Please cheer for everyone—it is a great confidence builder!          </li>
+</ul>
+<h3>Where do I park?          </h3>
+<p>The lot northeast of the SRC, across from the outdoor tennis courts provides the best parking for this year's Kid's Triathlon. Parking will be free Friday afternoon and Saturday morning.
+          If you are dropping off a registration form, please use the 30 minute parking spots in the semi-circle north of the building.
+          
+          <a href="/recsports/events/Kids-Triathlon/Parking 2011.pdf" class="pdf">Printable Parking Map </a></p>
+<h3>What time should we be there?</h3>
+<p> We will be open at 6:30 am. Everyone will have their arms and legs numbered with a magic marker and bikes/transition area will need to be set up. Please do not come later than 7 am in order to be ready on time. Everyone (even the older age group) must be at the pool for instructions at 7:15 am. Parents and helpers are allowed in the transition area before the race begins to help organize the bike, helmet, towel and pile of clothes/shoes. Placement of bike in the transition area is on a first come, first placed basis. Once the race begins, only participants are allowed in the transition area. Competitors will not be allowed to remove their bike and/gear from the transition area until the last competitor has begun the run.</p>
+<h3> What if a child needs medical attention?  </h3>
+<p>We will have plenty of volunteers to contact the race directors who are CPR certified. EMS will be called.</p>
+<h3> What if a child does not finish?  </h3>
+<p>Please report this to a race volunteer so we will know. Please encourage the child to stay and enjoy the day and cheer on others.  </p>
+<h3>Are there awards?  </h3>
+<p>The emphasis of this triathlon is in the name—Fit4 Fun—so there will be no awards for the fastest competitors but each child will receive a medal around his or her neck as(s)he crosses the finish line. Please plan to stay after the event as we will have random drawings for some great prizes. We will post individual times by Monday afternoon on this website for those who wish to see how they did.  </p>
+<h3>What happens if the weather is bad?</h3>
+<p> The race will take place rain or shine, unless the conditions are severe enough to cancel because of safety concerns. The directors have the right to shorten distances or delete a section if safety is an issue.</p>
+
+          <h3>2017 Results</h3>
+          <p><a href="Kids Triathlon/17/17 Kids_Tri_Results 7-10 age.pdf" class="pdf">2017 Results Ages 7 - 10</a></p>
+          <p class="pdf"><a href="Kids Triathlon/17/17 Kids_Tri_Results 11-14 age.pdf" class="pdf">2017 Results Ages 11 - 14</a></p>
+          <h3 class="pdf">2017 Pictures</h3>
+          <p class="pdf"><a href="https://www.facebook.com/pg/tturecsports/photos/?tab=album&amp;album_id=1519530108105514" class="external">Check out the picture on the TTU Rec Sports Facebook page!</a></p>
+
+              
+
+                 
+
+
+           
+
+            
+            </section>
+
+
+
+  </section>
+  <!-- CONTENT END -->
+
+        
+
+
+
+  <?php include '../includes/ttu-body-bottom.php'; ?>
+
+  <script src="/js/jquery.flexslider-min.js"></script>
+            <script>
+                $(window).load(function() {
+                $('.flexslider').flexslider({
+                    animation: "slide",
+                    controlNav: "thumbnails"
+                });
+                });
+            </script>
+
+  </body>
+</html>
---
+++ events/lunarlope.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include '../includes/ttu-head.html'; ?>
+        <title>Lunar Lope | Rec Center | TTU</title>
+        <meta name="Description" content="">
+    </head>
+    <body>
+        <?php include '../includes/ttu-body-top.php'; ?>
+
+
+
+
+        <!-- CONTENT START -->
+        <section class="rec-header">
+            <div class="rec-header__background">
+              <div class="top-left-triangle"></div>
+            </div>
+            <h1>Lunar Lope</h1>
+            <div class="rec-header__image">
+              <div class="top-left-triangle">
+                <div class="triangle-image" style="background-image: url('/images/ttu-aerial-1.jpg');"></div>
+              </div>
+            </div>
+        </section>
+        <section class="rec-sidebar">
+
+        </section>
+
+        <section class="rec-body">
+            <section>
+              <h2>Lunar Lope Fun Run 2018</h2>
+              
+            
+
+            <p>The Lunar Lope is a 5k Run or 1 Mile Walk for Texas Tech Recreation Members or interested community participants!</p>
+
+            <h3>Date</h3>
+            <p>Friday, June 9th</p>
+            <p>Race Starts at 10:00pm</p>
+
+            <h3>Place</h3>
+            <p>Race Day Registration and race start will be outside at the bonfire pit by the Leisure Pool South Entrance.</p>
+
+            <h3>Map</h3>
+            <p><img src="lunarlope/2017/map.jpeg" width="100%" height="auto"/></p>
+
+            <h3>Registration Information</h3>
+            <p>On-site Registration and Check-in from 9:00pm - 9:30pm</p>
+            <p>Pre-register at  the Fit/Well Office in the SRC or on <a href="https://register.recsports.ttu.edu" class="external">http://register.recsports.ttu.edu</a>For step by step instructions -----&gt; <a href="lunarlope/2017/Fusion online registration Lunar Lope.pdf" class="pdf">Click Here</a></p>
+            <ul>
+              <li>Texas Tech Rec Center Members : $9</li>
+              <li>Community Members: $18</li>
+            </ul>
+
+            <p>Registration includes dryfit t-shirt for first 150 participants. Register online by May 29th to be assured tshirt size. Children’s shirt sizes available ONLY until May 29th. Registrants after May 29th are not guaranteed correct tshirt size.</p>
+
+            <h3>Registration Form</h3>
+            <p><a href="lunarlope/2017/Lunar Lope EntryForm 2017.pdf" class="pdf">Click Here for the Registration Form </a></p>
+
+            <h3>2017 Lunar Lope Sponsor</h3>
+            <p>Thanks for Michael King, P.C.</p>
+            <p><img src="lunarlope/2016/lunar_logo_web.jpg" width="740" height="774" alt=""/></p>
+            <h3><strong>Running Map</strong></h3>
+            <p><a href="lunarlope/2016/lunar lope map 16.pdf" class="pdf">Click Here for the 5K Running Map</a></p>
+            <p><a href="../fitwell/images/running/Urbanovsky-Park-1.1mile.pdf" class="pdf">Click Here for the 1 Mile Walking Map</a></p>
+
+
+           
+
+            
+            </section>
+
+
+
+  </section>
+  <!-- CONTENT END -->
+
+        
+
+
+
+  <?php include '../includes/ttu-body-bottom.php'; ?>
+
+  <script src="/js/jquery.flexslider-min.js"></script>
+            <script>
+                $(window).load(function() {
+                $('.flexslider').flexslider({
+                    animation: "slide",
+                    controlNav: "thumbnails"
+                });
+                });
+            </script>
+
+  </body>
+</html>
